dbe2928de0a40e2485794b385226019d7a33741b
Christof Ruescher <rueschec@edu.htl-villach.at>
2019-10-14 12:20:32 +0100

    no message

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            //'userrole' => ['required', 'string'],
         ]);
     }
 
@@ -67,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'userrole' => $data['userrole'],
         ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,18 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="userrole" class="col-md-4 col-form-label text-md-right">{{ __('Userrole') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="userrole">
+                                    <option value = "student" selected>Student</option>
+                                    <option value = "teacher">Teacher</option>
+                                    <option value = "admin">Admin</option>
+                                </select>
+                            </div>
+                        </div><br>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
