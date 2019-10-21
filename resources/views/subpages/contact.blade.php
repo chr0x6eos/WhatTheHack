6c1de58618f9b5f973900564605facd86f4577be
sandr <sandr@edu.htl-villach.at>
2019-10-21 12:13:27 +0200

    subpages - contact and agb routes assigned

--- resources/views/subpages/contact.blade.php
+++ resources/views/subpages/contact.blade.php
@@ -1,1 +1,8 @@
 @extends ('layouts.app')
+<div class="card">
+    <div class="card-header">{{ __('Information') }}</div>
+
+    <div class="card-body">
+        This is a Demotext!
+    </div>
+</div>
--- resources/views/welcome_new.blade.php
+++ resources/views/welcome_new.blade.php
@@ -1,55 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="wrapper">
-        <!-- Sidebar -->
-        <nav id="sidebar">
+    <div class="card">
+        <div class="card-header">{{ __('Information') }}</div>
 
-            <ul class="list-unstyled components">
-
-                <li>
-                    <a href="#">Demopage 1</a>
-                </li>
-                <li>
-                    <a href="#">Demopage 2</a>
-                </li>
-                <li>
-                    <a href="#">Demopage 3</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">About</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                        <li>
-                            <a href="#">Terms of use</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-        </nav>
-        <!-- Page Content -->
-        <div id="content">
-
-            <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-
-                    <button type="button" id="sidebarCollapse" class="btn btn-info">
-                        <i class="fas fa-align-left"></i>
-                        <span>Toggle Sidebar</span>
-                    </button>
-                </div>
-            </nav>
-            -->
-        </div>
-        <div class="card">
-            <div class="card-header">{{ __('Information') }}</div>
-
-            <div class="card-body">
-                This is a Demotext!
-            </div>
+        <div class="card-body">
+            This is a Demotext!
         </div>
     </div>
 @endsection
