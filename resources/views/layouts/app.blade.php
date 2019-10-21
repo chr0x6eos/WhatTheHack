bde92c5348f61f00f72cb5f0372c36ed942080a1
sandr <sandr@edu.htl-villach.at>
2019-10-21 11:49:16 +0200

    new welcome-site and a menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <!-- Styles
     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::asset('resources/css/stylesheet.css') }}">
 </head>
 <body>
     <div id="app">
