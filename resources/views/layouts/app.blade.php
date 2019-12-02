068ec5ee42aad75e7e20ebaa8b22456ade658adc
Sandro <omanns@edu.htl-villach.at>
2019-12-02 10:10:00 +0100

    menu styling finished 01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,14 @@
     <!-- <script type="text/javascript" src="{{ URL::asset('js/addons/datatables-select.min.js') }}"></script> -->
     <script src="{{ asset('js/actions.js') }}"></script>
 
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -50,7 +58,7 @@
 </head>
 <body>
 
-    <div id="app">
+    <div id="content">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 
             <div class="navbar-toggle">
@@ -75,10 +83,13 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+                <!--
                 <button type="button" id="dismiss" class="btn btn-info">
                     <i class="fas fa-align-left"></i>
                     <span class="navbar-toggler-icon"></span>
                 </button>
+-->
+
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -136,7 +147,6 @@
 
             <div class="sidebar-header">
                 <h4>WhatTheHack</h4>
-
             </div>
 
             <ul class="list-unstyled components">
