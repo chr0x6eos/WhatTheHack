cf303764d5c0de7cf89fb65efbc3cbb02d8013af
Simon <simon.prast.sp@gmail.com>
2019-12-09 11:48:44 +0100

    Add Authorization to Controllers
    Add authorization middleware calls to several controllers:
    - ManageUserController needs the role:admin check to succeed.
    - ChallengeController needs the auth check to succeed.

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class ChallengeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/ManageUserController.php
+++ app/Http/Controllers/ManageUserController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class ManageUserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('role:admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
