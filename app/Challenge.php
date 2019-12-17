d27c0b1b860b941eaab633c3762bd9200e9e5091
Simon <simon.prast.sp@gmail.com>
2019-12-17 10:45:28 +0100

    Fix merge error
    Add the validCategory() function to the Challenge model. This was override during the merge process.

--- app/Challenge.php
+++ app/Challenge.php
@@ -7,11 +7,22 @@ use Illuminate\Database\Schema\Blueprint;
 
 class Challenge extends Model
 {
-    //Check if inputted difficulty is valid
+    // Check if inputted difficulty is valid
     public function validDifficulty($difficulty)
     {
         $validDiffs = ['tatu','easy','medium','hard','insane'];
         if(in_array($difficulty,$validDiffs))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Check if inputted category is valid
+    public function validCategory($category)
+    {
+        $validCat = ['pwn','web','forensic','reversing','crypto','misc'];
+        if(in_array($category, $validCat))
         {
             return true;
         }
