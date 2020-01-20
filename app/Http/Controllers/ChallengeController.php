deb5964cf02d7471e7b76182c376b372a2637a56
Simon <simon.prast.sp@gmail.com>
2020-01-20 11:11:38 +0100

    Add Verified-email Check to Auth Middleware
    Add a check if the user has a verified email to the role auth middleware.
    This only applies to the role check middleware; When checking for any authenticated user with the auth middleware, you can use the 'verified' middleware within your application in addition to auth:
    $this->middleware('auth', 'verified');

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -11,7 +11,7 @@ class ChallengeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', 'verified');
     }
 
     /**
