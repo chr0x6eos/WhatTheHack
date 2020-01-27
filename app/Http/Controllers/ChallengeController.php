81c9a7ae9364cf332fb7b064a0091eab08acc3f7
Simon <simon.prast.sp@gmail.com>
2020-01-27 11:19:24 +0100

    Fix bug with middleware calls
    Multiple middleware must be called as an array instead of two string arguments. Sorry.

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -12,7 +12,7 @@ class ChallengeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', 'verified');
+        $this->middleware(['auth', 'verified']);
     }
 
     /**
--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -16,7 +16,7 @@ class ClassroomController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', 'verified');
+        $this->middleware(['auth', 'verified']);
     }
 
 
