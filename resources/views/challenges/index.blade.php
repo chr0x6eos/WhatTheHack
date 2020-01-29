869b5679fbac5d4c634da73e762b31423ecd05dd
hehnm <hehnm@edu.htl-villach.at>
2020-01-29 09:48:38 +0100

    Add feature
    Filtering challenges is now possible

--- resources/views/challenges/index.blade.php
+++ resources/views/challenges/index.blade.php
@@ -1,93 +1,63 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-header font-weight-bold">{{ __('Challenges') }}</div>
-
+        <div class="card row " style="float: left; width: 150px; margin-right: 30px">
+            <div class="card-header">Filter</div>
             <div class="card-body">
-                @if(isset($challenges) && sizeof($challenges) > 0)
-
-                    <table id="challenge" border="1"class="table table-bordered">
-                        <thead>
-                        <th>Name</th>
-                        <th>Difficulty</th>
-                        <th>Author</th>
-                        <th>Status</th>
-                        <th colspan="1">Info</th>
-                        @if(Auth::user() && ( Auth::user()->hasRole("admin") || Auth::user()->hasRole("teacher")))
-                            <th colspan="1">Edit</th>
-                        @endif
-                        </thead>
-                        <tbody>
-                        @foreach($challenges as $challenge)
-                            @if($challenge->active==1)
-                                @if(Auth::user()->solvedChallenge($challenge->id))
-                                    @php//TODO:Change colour to match theme @endphp
-                                  <tr bgcolor="#f0f8ff">
-                                    <td>{{ $challenge->name }}</td>
-                                    <td>{{ $challenge->difficulty }}</td>
-                                    <td>{{ $challenge->author }}</td>
-                                    <td>@if($challenge->active)
-                                            Enabled
-                                        @else
-                                            Disabled
-                                        @endif
-                                    </td>
-                                    <td><a href="{{ route('challenges.show', $challenge->id) }}" class="btn btn-info">More info</a></td>
-                                    @if(Auth::user() && ( Auth::user()->hasRole("admin") || Auth::user()->hasRole("teacher")))
-                                    <td>
-                                        <a href="{{ route('challenges.edit', $challenge->id) }}" class="btn btn-outline-danger">Edit</a>
-                                    </td>
-                                    @endif
-                                 </tr>
-                                @else
-                                    <tr>
-                                    <td>{{ $challenge->name }}</td>
-                                    <td>{{ $challenge->difficulty }}</td>
-                                    <td>{{ $challenge->author }}</td>
-                                    <td>@if($challenge->active)
-                                            Enabled
-                                        @else
-                                            Disabled
-                                        @endif
-                                    </td>
-                                    <td><a href="{{ route('challenges.show', $challenge->id) }}" class="btn btn-info">More info</a></td>
-                                    @if(Auth::user() && ( Auth::user()->hasRole("admin") || Auth::user()->hasRole("teacher")))
-                                        <td>
-                                            <a href="{{ route('challenges.edit', $challenge->id) }}" class="btn btn-outline-danger">Edit</a>
-                                        </td>
-                                    @endif
-                                    </tr>
-                                @endif
-                            @endif
-                        @endforeach
-                        </tbody>
-                    </table>
-                @else
-                    <h3>No challenges yet!</h3>
-                @endif
-                <br>
-                {{-- Only administrators or teachers are allowed to edit challenges --}}
-                @if(Auth::user() && ( Auth::user()->hasRole("admin") || Auth::user()->hasRole("teacher")))
-                    <a href="{{ route('challenges.create') }}" class="btn btn-info">Add new challenge</a>
-                @endif
+                <div class="form-group row"><input type="submit" class="btn btn-elegant" id="solved_button" value="Solved" /></div>
+                <div class="form-group row"><input type="submit" class="btn btn-elegant" id="unsolved_button" value="Unsolved" /></div>
+                <div class="form-group row"><input type="submit" class="btn btn-elegant" id="all_button" value="All" /></div>
             </div>
+        </div>
+        <div class="container my-shuffle-container row " style="width: 100%; margin-right: 40px">
+            @foreach($challenges as $challenge)
+                @if($challenge->active==1)
+                    @if(Auth::user()->solvedChallenge($challenge->id))
+                        @php//TODO:Change colour to match theme @endphp
+                        <div class="card picture-item " data-groups='["solved"]' style="background-color: #0C9A9A">
+                            <div class="card-header"><a href="{{route('challenges.show',$challenge->id)}}">{{$challenge->name}} - {{$challenge->category}}</a>
+                            <p style="float: right;color: black">Total solves: {{$challenge->solves($challenge->id)}}</p>
+                            </div>
+                            <div class="card-body">
+                                Author: {{$challenge->author}}
+                                <br>
+                                {{$challenge->description}}
+                            </div>
+                        </div>
+                    @else
+                        <div class="picture-item" data-groups='["unsolved"]'>
+                            <div class="card-header"><a href="{{route('challenges.show',$challenge->id)}}">{{$challenge->name}} - {{$challenge->category}}</a>
+                                <p style="float: right;color: black">Total solves: {{$challenge->solves($challenge->id)}}</p>
+                            </div>
+                            <div class="card-body">
+                                Author: {{$challenge->author}}
+                                <br>
+                                {{$challenge->description}}
+                            </div>
+                        </div>
+                    @endif
+                @endif
+             @endforeach
+        </div>
             <script>
-                $(document).ready(
-                    function () {
-                        $('#challenge').DataTable( {
-                            "paging": true,
-                            "info":false,
-                            "aoColumns": [
-                                null,
-                                null,
-                                null,
-                                { "bSearchable": false, "orderable": false },
-                                { "bSearchable": false, "orderable": false },
-                                { "bSearchable": false, "orderable": false },
-                                // { "bSearchable": false, "orderable": false }
-                            ]
-                        });
+                $(document).ready(function() {
+                    var Shuffle = window.Shuffle;
+                    var element = document.querySelector('.my-shuffle-container');
+                    var sizer = element.querySelector('.my-sizer-element');
+                    var shuffleInstance = new Shuffle(element, {
+                        itemSelector: '.picture-item',
                     });
+                    $("#solved_button").on("click", function () {
+                        shuffleInstance.filter("solved");
+                    });
+                    $("#unsolved_button").on("click", function () {
+                        shuffleInstance.filter("unsolved");
+                    });
+                    $("#all_button").on("click", function () {
+                        shuffleInstance.filter();
+                    });
+                });
             </script>
+        </div>
+    </div>
 @endsection
