f119b6d48f350818e81f4f9f41dbca15aef7809d
Simon <simon.prast.sp@gmail.com>
2020-01-29 10:09:22 +0100

    Redirect Login to Index Route
    Redirect access of the 'login' route to the 'main' route, as this is our login page.

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::get('/contact', function () {
 });
 
 Auth::routes(['verify' => true]);
+
+Route::get('/login', function () {
+    return redirect()->route('main');
+})->name('login');
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/challenges','ChallengeController@index')->name('challenges.index');
 Route::get('/classroom', 'ClassroomController@index')->name('classroom.index');
