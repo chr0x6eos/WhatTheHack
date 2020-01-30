5f4127c36b407a50577a3c4a809daa3ae2ec3c41
Sandro <omanns@edu.htl-villach.at>
2020-01-30 10:24:58 +0100

    Style update
    New show my Challenges view

--- resources/views/classroom/showChallenges.blade.php
+++ resources/views/classroom/showChallenges.blade.php
@@ -1,98 +1,87 @@
 @extends('layouts.app')
-
 @section('content')
-
-    <div class="container" style="float: right; width: 15%; margin-right: 40px; position: relative;">
-        <div class="card" style="background-color: dimgrey">
-            <div class="card-header ">Solved</div>
-            <div class="card-body">
-                <div class="form-group row"><input type="submit" class="btn " id="solved_button" value="Solved" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="unsolved_button" value="Unsolved" /></div>
-                <div class="form-group row "><input type="submit" class="btn " id="all_button" value="All" /></div>
-            </div>
-        </div>
-        <div class="card" style="background-color: dimgrey">
-            <div class="card-header ">Filter</div>
-            <div class="card-body">
-                <div class="form-group row"><input type="submit" class="btn " id="misc_button" value="misc" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="web_button" value="Web Exploitation" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="forensic_button" value="Forensic" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="reversing_button" value="Reverse Engineering" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="crypto_button" value="Cryptography" /></div>
-                <div class="form-group row"><input type="submit" class="btn " id="pwn_button" value="Binary Exploitation" /></div>
+    <div id="landing" class="py-5">
+        <div data-test="container" class="container">
+            <div data-test="row" class="row">
+                <div data-test="col" class="col-xl-9">
+                        <div data-test="card" class="card-body">
+                            <p class="h2" style="display: inline"> Challenges of Classroom </p><p class="h2" style="color: #01C851; display: inline">{{$classroom->classroom_name}}</p>
+                            <br>
+                            <br>
+                            @foreach($classroom->challenges as $challenge)
+                                @if($challenge->active==1)
+                                    @if(Auth::user()->solvedChallenge($challenge->id))
+                                        <div class="card">
+                                            <a href="{{route('challenges.show',$challenge->id)}}" class="challenge_name_link">
+                                                <div class="card-header">
+                                                    <p class="challenge_name">{{$challenge->name}}</p>
+                                                    <p class="total_solves">Total solves: {{$challenge->solves($challenge->id)}}</p>
+                                                </div>
+                                            </a>
+                                            <div class="card-body">
+                                                <p>Author: {{$challenge->author}}</p>
+                                                <p>{{$challenge->description}}</p>
+                                            </div>
+                                        </div>
+                                    @else
+                                        <div class="card">
+                                            <a href="{{route('challenges.show',$challenge->id)}}">
+                                                <div class="card-header">
+                                                    <p style="display: inline">{{$challenge->name}}</p>
+                                                    <p class="total_solves">Total solves: {{$challenge->solves($challenge->id)}}</p>
+                                                </div>
+                                            </a>
+                                            <div class="card-body">
+                                                <p>Author: {{$challenge->author}}</p>
+                                                <p>{{$challenge->description}}</p>
+                                            </div>
+                                        </div>
+                                        <br>
+                                    @endif
+                                @endif
+                            @endforeach
+                        </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="container" style="width: 1000px">
-        <h1>You are in this classroom: {{$classroom->classroom_name}}</h1>
-        <div class="my-shuffle-container row " style="width: 100%; margin-right: 40px">
-            @foreach($classroom->challenges as $challenge)
-                @if($challenge->active==1)
-                    @if(Auth::user()->solvedChallenge($challenge->id))
-                        @php//TODO:Change colour to match theme @endphp
-                        <div class="card picture-item   " data-groups='["solved","{{$challenge->category}}"]' style="background-color: dimgrey" >
-                            <div class="card-header" style="background-color: #0E9A00"><a href="{{route('challenges.show',$challenge->id)}}">{{$challenge->name}} - {{$challenge->category}}</a>
-                                <p style="float: right;color: black">Total solves: {{$challenge->solves($challenge->id)}} ~solved</p>
-                            </div>
-                            <div class="card-body">
-                                Author: {{$challenge->author}}
-                                <br>
-                                {{$challenge->description}}
-                            </div>
-                        </div>
-                    @else
-                        <div class="picture-item" data-groups='["unsolved","{{$challenge->category}}"]' style="background-color: dimgrey">
-                            <div class="card-header"><a href="{{route('challenges.show',$challenge->id)}}">{{$challenge->name}} - {{$challenge->category}}</a>
-                                <p style="float: right;color: black">Total solves: {{$challenge->solves($challenge->id)}}</p>
-                            </div>
-                            <div class="card-body">
-                                Author: {{$challenge->author}}
-                                <br>
-                                {{$challenge->description}}
-                            </div>
-                        </div>
-                    @endif
-                @endif
-            @endforeach
-        </div>
-        <script>
-            $(document).ready(function() {
-                var Shuffle = window.Shuffle;
-                var element = document.querySelector('.my-shuffle-container');
-                var sizer = element.querySelector('.my-sizer-element');
-                var shuffleInstance = new Shuffle(element, {
-                    itemSelector: '.picture-item',
-                });
-                $("#solved_button").on("click", function () {
-                    shuffleInstance.filter("solved");
-                });
-                $("#unsolved_button").on("click", function () {
-                    shuffleInstance.filter("unsolved");
-                });
-                $("#all_button").on("click", function () {
-                    shuffleInstance.filter();
-                });
-                $("#misc_button").on("click", function () {
-                    shuffleInstance.filter("misc");
-                });
-                $("#web_button").on("click", function () {
-                    shuffleInstance.filter("web");
-                });
-                $("#forensic_button").on("click", function () {
-                    shuffleInstance.filter("forensic");
-                });
-                $("#crypto_button").on("click", function () {
-                    shuffleInstance.filter("crypto");
-                });
-                $("#pwn_button").on("click", function () {
-                    shuffleInstance.filter("pwn");
-                });
-                $("#reversing_button").on("click", function () {
-                    shuffleInstance.filter("reversing");
-                });
+    <script>
+        $(document).ready(function() {
+            var Shuffle = window.Shuffle;
+            var element = document.querySelector('.my-shuffle-container');
+            var sizer = element.querySelector('.my-sizer-element');
+            var shuffleInstance = new Shuffle(element, {
+                itemSelector: '.picture-item',
             });
-        </script>
-    </div>
+            $("#solved_button").on("click", function () {
+                shuffleInstance.filter("solved");
+            });
+            $("#unsolved_button").on("click", function () {
+                shuffleInstance.filter("unsolved");
+            });
+            $("#all_button").on("click", function () {
+                shuffleInstance.filter();
+            });
+            $("#misc_button").on("click", function () {
+                shuffleInstance.filter("misc");
+            });
+            $("#web_button").on("click", function () {
+                shuffleInstance.filter("web");
+            });
+            $("#forensic_button").on("click", function () {
+                shuffleInstance.filter("forensic");
+            });
+            $("#crypto_button").on("click", function () {
+                shuffleInstance.filter("crypto");
+            });
+            $("#pwn_button").on("click", function () {
+                shuffleInstance.filter("pwn");
+            });
+            $("#reversing_button").on("click", function () {
+                shuffleInstance.filter("reversing");
+            });
+        });
+    </script>
 @endsection
 
 
