aff570838fd2babb9ee0c7d47c56af88d4c1c32c
Simon Possegger <posseggs@edu.htl-villach.at>
2020-02-03 11:53:43 +0100

    Change button color
    Changed button color on classroom-ranking view

--- resources/views/ranking/classroomRanking.blade.php
+++ resources/views/ranking/classroomRanking.blade.php
@@ -6,7 +6,7 @@
         <div class="card">
             <div class="card-header font-weight-bold">{{ __('Classroom Ranking') }}</div>
             <div class="card-body">
-                <select class="btn btn-secondary dropdown-toggle">
+                <select class="btn btn-outline-dark dropdown-toggle">
                     <option value="-1"> All Classrooms</option>
                     @foreach($classrooms as $classroom)
                         <option value="{{ $classroom->id }}">{{ $classroom->classroom_name }}</option>
